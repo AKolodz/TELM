8d85847071d10450caf7efb9e9160d9636d65691
A.T.Kołodziejczyk <a.t.kolodziejczyk@gmail.com>
2017-11-23 02:14:35 +0100

    WIP: I was able to get permission to my account and add event by intent
    Great ball of mud that does nothing, but something is starting to work.

--- app/src/main/java/telm/krzeminska/kolodziejczyk/exeminecalendar/EventList/mvp/EventListModel.kt
+++ app/src/main/java/telm/krzeminska/kolodziejczyk/exeminecalendar/EventList/mvp/EventListModel.kt
@@ -1,11 +1,25 @@
 package telm.krzeminska.kolodziejczyk.exeminecalendar.EventList.mvp
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.provider.CalendarContract.Calendars
+import android.support.v4.app.ActivityCompat
 import telm.krzeminska.kolodziejczyk.exeminecalendar.model.Event
+
 
 /**
  * Created by aleksander on 22.11.17.
  */
 class EventListModel : EventListMVP.Model {
+
+
+    init {
+
+    }
+
     override fun getEvents(): MutableList<Event> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
